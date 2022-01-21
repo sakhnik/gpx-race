f873210056dd063709365d7831b50be75b71809f
Anatolii Sakhnik <sakhnik@gmail.com>
2022-01-21 12:45:01 +0200

    Add one more track

--- render.py
+++ render.py
@@ -17,7 +17,8 @@ def get_data(fname):
     return points
 
 
-points = get_data('/tmp/track.gpx')
+points = get_data('track.gpx')
+points2 = get_data('track2.gpx')
 
 latitude = sum(p[0] for p in points)/len(points)
 longitude = sum(p[1] for p in points)/len(points)
@@ -28,18 +29,33 @@ my_map = folium.Map(location=[latitude, longitude], zoom_start=14)
 
 data = {
     "type": "FeatureCollection",
-    "features": [{
+    "features": []
+}
+
+
+def add_track(points, dt):
+    feature = {
         'type': 'Feature',
         'geometry': {
             'type': 'LineString',
             'coordinates': [[p[1], p[0]] for p in points]
         },
         'properties': {
-            'times': [p[3].timestamp() * 1000 for p in points],
+            'times': [(p[3].timestamp() + dt) * 1000 for p in points],
+            'style': {
+                'color': "#f00",
+                'opacity': 0.5,
+            },
         }
-    }]
-}
+    }
+    data['features'].append(feature)
 
-TimestampedGeoJson(data, period='PT1S').add_to(my_map)
+
+dt = (points[0][3] - points2[0][3]).total_seconds()
+
+add_track(points, 0)
+add_track(points2, dt)
+
+TimestampedGeoJson(data, period='PT5S', transition_time=50).add_to(my_map)
 
 my_map.save('test.html')
