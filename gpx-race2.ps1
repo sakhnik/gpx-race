a8c19f64bc9645a021c272c6edacc68e007f6fc6
Anatolii Sakhnik <sakhnik@gmail.com>
2022-01-30 23:08:18 +0200

    Add launch script on Windows

---
+++ gpx-race2.ps1
@@ -0,0 +1,5 @@
+
+virtualenv venv
+venv/Scripts/activate
+pip install -r requirements.txt
+voila gpx-race2.ipynb
--- gpx-race2.sh
+++ gpx-race2.sh
@@ -2,5 +2,5 @@
 
 virtualenv /tmp/venv
 source /tmp/venv/bin/activate
-
 pip install -r requirements.txt
+voila gpx-race2.ipynb
